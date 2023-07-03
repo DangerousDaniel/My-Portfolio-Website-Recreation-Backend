c18d70dc3ec9323c65696ab5457da7e10519822b
DangerousDaniel <danielcox996@gmail.com>
2023-07-03 20:17:58 -0300

    static data created

--- MyWebsiteRecreationDatabase/MyWebsiteRecreationDatabase/settings.py
+++ MyWebsiteRecreationDatabase/MyWebsiteRecreationDatabase/settings.py
@@ -139,9 +139,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/static'
+MEDIA_URL = 'static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = 'static/static'
+MEDIA_ROOT = 'static/media'
