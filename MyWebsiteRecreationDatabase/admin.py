335e66f51e3c5a4439a5eba9e6db653c6c07893c
DangerousDaniel <danielcox996@gmail.com>
2023-07-04 01:32:22 -0300

    Last Updated Comment header dates updated

--- MyWebsiteRecreationDatabase/admin.py
+++ MyWebsiteRecreationDatabase/admin.py
@@ -2,7 +2,7 @@
     Project Name: My Portfolio Website Recreation
     Authors: Daniel Cox
     Created Date: April 21, 2023
-    Last Updated: May 25, 2023
+    Last Updated: July 3, 2023
     Description: This is the class for registering your all table to the Django Admin. 
     Notes:
     Resources: 
