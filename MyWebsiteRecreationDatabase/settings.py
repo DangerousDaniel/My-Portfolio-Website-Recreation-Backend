ff8b875aebfca2fefc09617f5a9bbc02e3680edd
DangerousDaniel <danielcox996@gmail.com>
2023-07-03 22:24:35 -0300

    add env variables

--- MyWebsiteRecreationDatabase/settings.py
+++ MyWebsiteRecreationDatabase/settings.py
@@ -31,10 +31,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f4%t-p9@m4)q7nu^d*4k0$+*eind)^_mvr7jzvg^n58m%$wylh'
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = ['localhost','website-recreation-database-315da4046bd5.herokuapp.com']
 
