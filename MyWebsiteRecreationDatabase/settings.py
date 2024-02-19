26f6f7b111d3b274dee559f53b37b54927ff461c
DangerousDaniel <danielcox996@gmail.com>
2024-02-19 16:33:24 -0400

    Update settings.py

--- MyWebsiteRecreationDatabase/settings.py
+++ MyWebsiteRecreationDatabase/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_POST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
